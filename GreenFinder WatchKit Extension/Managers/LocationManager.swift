64b6ee50a2dac30b8abbd995a72429a7ff834098
Erik Dreifaldt <erik.dreifaldt@gmail.com>
2021-07-20 00:22:11 +0200

    first commit

---
+++ GreenFinder WatchKit Extension/Managers/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  GreenFinder WatchKit Extension
+//
+//  Created by Erik Dreifaldt on 2021-07-19.
+//
+
+import Foundation
---
+++ GreenFinder WatchKit Extension/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  GreenFinder WatchKit Extension
+//
+//  Created by Erik Dreifaldt on 2021-06-13.
+//
+
+import Foundation
